1cda9a63fdedf3b7b4e62f1b60ee5dc9b6d8c947
Rohit kumar <rohit97751@gmail.com>
2021-11-26 00:14:35 +0530

    arrow down icon

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,21 @@
-const search = document.getElementById("search");
 
-search.addEventListener("click", () => {
-  const search_Bar = document.getElementById("searchbar");
-  search_Bar.classList.toggle("active");
-});
 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
