d4e1fce3224ba216481b10a15ca49224c93e6ade
Rohit kumar <rohit.kumar.neosoftmail@gmail.com>
2021-12-20 17:24:16 +0530

    Desktop Hero

--- Coromandel/app/js/app.js
+++ Coromandel/app/js/app.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Header Information section
+  const dropdown = document.getElementById("dropdown");
+  dropdown.addEventListener("click", () => {
+    const dropdown_content = document.getElementById("dropdown-content");
+    dropdown_content.classList.toggle("active-dropdown-content");
+  });
+
+  // Header Search Bar
   const bx_search = document.querySelector(".bx-search");
-    bx_search.addEventListener("click", (e) => {
+  bx_search.addEventListener("click", (e) => {
     const search = document.getElementById("search");
-    search.classList.toggle("active");
+    search.classList.toggle("active-search");
   });
 });
