816b3a6d3765ac3d7e81f57c11189d8a162ba040
Rohit kumar <rohit.kumar.neosoftmail@gmail.com>
2021-12-29 18:48:20 +0530

    Footer bottom pading, GROMOR Scroll, Awards screen 992-size, Slider icon, Hero image, Co. Growth Cards

--- Coromandel/app/js/app.js
+++ Coromandel/app/js/app.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Header Information section
-  const dropdown = document.getElementById("dropdown");
-  dropdown.addEventListener("click", () => {
-    const dropdown_content = document.getElementById("dropdown-content");
-    dropdown_content.classList.toggle("active-dropdown-content");
-  });
+  
 
   // Header Search Bar
   const bx_search = document.querySelector(".bx-search");
