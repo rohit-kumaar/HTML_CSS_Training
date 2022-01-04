ef559c00dd0b1f80344855d1f67496a6d30a1f3b
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-01-04 19:10:19 +0530

    login page with responsive

---
+++ Deloitte/app/js/app.js
@@ -0,0 +1,12 @@
+// let number = 15
+
+// console.log( number / 16);
+// console.log(
+
+// 405-320
+
+// );
+
+document.addEventListener("DOMContentLoaded", function () {
+  let input = document.querySelector(".input");
+});
