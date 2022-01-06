d590c1249dafeae7bf533f3f0a896b361f285f0d
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-01-06 17:04:06 +0530

    Profile settings

--- Deloitte/app/js/dashboard.js
+++ Deloitte/app/js/dashboard.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   let sidebar = document.querySelector(".sidebar");
   const closeBtn = document.getElementById("btn");
+  const log_out = document.getElementById("log_out");
+  const settings = document.getElementById("settings");
 
   closeBtn.addEventListener("click", () => {
     sidebar.classList.toggle("open");
     menuBtnChange();
+  });
+
+  log_out.addEventListener("click", () => {
+    settings.classList.toggle("active");
   });
 
   function menuBtnChange() {
