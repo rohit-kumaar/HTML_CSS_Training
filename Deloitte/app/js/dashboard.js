c7365e6e385f62e84fb42f0afc348666776a0c1e
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-01-06 16:14:57 +0530

    As per design sidebar

--- Deloitte/app/js/dashboard.js
+++ Deloitte/app/js/dashboard.js
@@ -1,23 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   let sidebar = document.querySelector(".sidebar");
-  let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
+  const closeBtn = document.getElementById("btn");
 
   closeBtn.addEventListener("click", () => {
     sidebar.classList.toggle("open");
     menuBtnChange();
   });
 
-  searchBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-    menuBtnChange();
-  });
-
   function menuBtnChange() {
     if (sidebar.classList.contains("open")) {
-      closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+      closeBtn.classList.replace("bx-chevron-right", "bx-chevron-left");
     } else {
-      closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+      closeBtn.classList.replace("bx-chevron-left", "bx-chevron-right");
     }
   }
 });
