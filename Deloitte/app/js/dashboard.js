d67dc0a8947d93d25b32d54d01a1a7d8334f9042
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-01-13 17:16:16 +0530

    Assessment Master

--- Deloitte/app/js/dashboard.js
+++ Deloitte/app/js/dashboard.js
@@ -9,6 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const main = document.getElementById("main");
   const section = main.querySelectorAll("section");
 
+  function menuBtnChange() {
+    if (sidebar.classList.contains("close")) {
+      closeBtn.classList.replace("bx-chevron-left", "bx-chevron-right");
+    } else {
+      closeBtn.classList.replace("bx-chevron-right", "bx-chevron-left");
+    }
+  }
+
   closeBtn.addEventListener("click", () => {
     sidebar.classList.toggle("close");
     menuBtnChange();
@@ -18,14 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
     settings.classList.toggle("active");
   });
 
-  function menuBtnChange() {
-    if (sidebar.classList.contains("close")) {
-      closeBtn.classList.replace("bx-chevron-left", "bx-chevron-right");
-    } else {
-      closeBtn.classList.replace("bx-chevron-right", "bx-chevron-left");
-    }
-  }
-
   function showPage(page) {
     document.querySelectorAll("section").forEach((section) => {
       section.style.display = "none";
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(`#${page}`).style.display = "block";
   }
 
-  document.querySelectorAll("li").forEach((li) => {
+  document.querySelectorAll("aside ul li").forEach((li) => {
     li.addEventListener("click", function () {
       showPage(this.dataset.page);
     });
@@ -47,5 +47,26 @@ document.addEventListener("DOMContentLoaded", function () {
   notificationBell.addEventListener("click", () => {
     notification.classList.toggle("active");
     section[0].classList.toggle("active");
+    section[1].classList.toggle("active");
+    section[2].classList.toggle("active");
+    section[3].classList.toggle("active");
+    section[4].classList.toggle("active");
+    section[5].classList.toggle("active");
   });
+
+  // const assessmentMainListStatus = document.querySelector(
+  //   ".assessment-main-list--status"
+  // );
+
+  // const formCheckInput = document.querySelector(".form-check-input");
+
+  // formCheckInput.addEventListener("click", () => {
+  //   if ((type = "checkbox")) {
+  //     console.log("ACTIVE");
+  //     assessmentMainListStatus.innerText = "ACTIVE";
+  //   } else if (type !== "checkbox") {
+  //     console.log("INACTIVE");
+  //     assessmentMainListStatus.innerText = "INACTIVE";
+  //   }
+  // });
 });
